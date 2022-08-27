1c3623fcf05aa54d1798fd3fa70083e61a8f0624
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-08-27 14:58:02 +0300

    rm client dir

--- client/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import Button from "@mui/material/Button";
-import React from 'react'
-
-export default function CustomButton() {
-  return (
-
-    <Button variant="contained"> GET BALANCE </Button>
-  )
-}
