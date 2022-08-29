112535339695c2da4a00d1e9ddf955cdeb768bb1
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-08-29 10:59:21 +0300

    added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Landing from "./components/Landing"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <>
-        <Landing/>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+      </Routes>
     </>
   )
 }
